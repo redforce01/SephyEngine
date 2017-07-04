e429e4e407f56a35af9d111e2afea7206678cac1
JaeJun Kim <redforce01@naver.com>
2017-07-05 03:35:46 +0900

    Update 17.07.05 Resource Replaced
    Update 17.07.05 Resource Replaced

--- DirectX_Engine/logViewer.cpp
+++ DirectX_Engine/logViewer.cpp
@@ -10,3 +10,28 @@ LogViewer::LogViewer()
 LogViewer::~LogViewer()
 {
 }
+
+bool LogViewer::initialize(Graphics * g, Input * i)
+{
+
+
+	return false;
+}
+
+void LogViewer::update(float frameTime)
+{
+	SystemUIDialog::update(frameTime);
+
+	if (getMouseOver() == false)
+		return;
+}
+
+void LogViewer::render()
+{
+	if (m_bVisible == false)
+		return;
+
+	SystemUIDialog::render();
+
+
+}
--- DirectX_Engine/logViewer.h
+++ DirectX_Engine/logViewer.h
@@ -5,12 +5,34 @@ class LogViewer;
 
 #include "systemUIDialog.h"
 
+namespace logViewerNS
+{
+	const UINT WIDTH = 400;				// width of treeViewer
+	const UINT HEIGHT = WINSIZEY;		// height of treeViewer
+	const UINT X = WINSIZEX - WIDTH;	// Viewer location X
+	const UINT Y = 0;					// Viewer location Y
+	const UINT MARGIN = 4;              // text margin from Viewer edge
+	const char FONT[] = "Courier New";  // Viewer font
+	const int FONT_HEIGHT = 14;         // height of the font in pixels
+	const COLOR_ARGB FONT_COLOR = graphicsNS::WHITE;    // color of console text
+	const COLOR_ARGB BACK_COLOR = SETCOLOR_ARGB(192, 26, 32, 44);    // backdrop color
+
+	const std::string TILE_FOLDER = "03_Tile";
+	const UINT BASIC_TILE_WIDTH = 128;
+	const UINT BASIC_TILE_HEIGHT = 64;
+	const UINT VIEWER_PAGE_ITEM_MAX = 36;
+
+	const int LEFT_BUTTON_ID = 100;
+	const int RIGHT_BUTTON_ID = 101;
+	const UINT PAGE_BUTTON_SIZE = 30;
+	const UINT PAGE_BUTTON_MARGIN = 0;
+}
 
 class LogViewer : public SystemUIDialog
 {
 private:
 	std::deque<std::string> m_arrLog;
-
+	
 
 public:
 	LogViewer();
@@ -19,6 +41,11 @@ public:
 	virtual bool initialize(Graphics* g, Input* i) override;
 	virtual void update(float frameTime) override;
 	virtual void render() override;
+
+	void addLog(std::string message)
+	{
+		m_arrLog.emplace_front(message);
+	}
 };
 
 #endif // !_LOGVIEWER_H
